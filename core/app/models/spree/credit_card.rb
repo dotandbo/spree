2b4d601bb8560b491e8f0efcaf8b08e1e625d0b7
Chris Clerville <chris@dotandbo.com>
2014-05-20 09:45:48 -0700

    skip expired card validation

--- core/app/models/spree/credit_card.rb
+++ core/app/models/spree/credit_card.rb
@@ -118,12 +118,13 @@ module Spree
     private
 
     def expiry_not_in_the_past
-      if year.present? && month.present?
-        time = "#{year}-#{month}-1".to_time
-        if time < Time.zone.now.to_time.beginning_of_month
-          errors.add(:base, :card_expired)
-        end
-      end
+      true
+      # if year.present? && month.present?
+      #   time = "#{year}-#{month}-1".to_time
+      #   if time < Time.zone.now.to_time.beginning_of_month
+      #     errors.add(:base, :card_expired)
+      #   end
+      # end
     end
   end
 end
