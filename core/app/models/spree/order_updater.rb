d8644e21ca3427835626076c75cf183a3f96c99c
Jonah Schwartz <jonah@schwartzdev.com>
2014-09-03 15:07:23 -0700

    allow orders with a single failed payment to be considered paid if another proper payment is made

--- core/app/models/spree/order_updater.rb
+++ core/app/models/spree/order_updater.rb
@@ -148,18 +148,13 @@ module Spree
     # paid          when +payment_total+ is equal to +total+
     # balance_due   when +payment_total+ is less than +total+
     # credit_owed   when +payment_total+ is greater than +total+
-    # failed        when most recent payment is in the failed state
     #
     # The +payment_state+ value helps with reporting, etc. since it provides a quick and easy way to locate Orders needing attention.
     def update_payment_state
       last_state = order.payment_state
-      if payments.present? && payments.last.state == 'failed'
-        order.payment_state = 'failed'
-      else
-        order.payment_state = 'balance_due' if order.outstanding_balance > 0
-        order.payment_state = 'credit_owed' if order.outstanding_balance < 0
-        order.payment_state = 'paid' if !order.outstanding_balance?
-      end
+      order.payment_state = 'balance_due' if order.outstanding_balance > 0
+      order.payment_state = 'credit_owed' if order.outstanding_balance < 0
+      order.payment_state = 'paid' if !order.outstanding_balance?
       order.state_changed('payment') if last_state != order.payment_state
       order.payment_state
     end
