30ddc2f0d4bbeada3cc66f964f04c1c951df73c2
Chris Hyun Ho Kim <ckim@dotandbo.com>
2015-04-29 09:34:26 -0700

    shipment address is already tracked

--- core/app/models/spree/stock/package.rb
+++ core/app/models/spree/stock/package.rb
@@ -98,7 +98,6 @@ module Spree
 
       def to_shipment
         shipment = Spree::Shipment.new
-        shipment.address = order.ship_address
         shipment.order = order
         shipment.stock_location = stock_location
         shipment.shipping_rates = shipping_rates
